700a80525f5aca11f91c5f2dc144894ad4e35432
David Hsiao <davidhsiao@cybersoft4u.com>
2021-03-05 14:05:00 +0800

    * 已可透過Retrofit取得TaipeiZoo網址回傳的內容(網址不包含parameter)

--- app/src/main/java/android/example/com/taipeizoointrodemo/networkApi/AreaApiService.kt
+++ app/src/main/java/android/example/com/taipeizoointrodemo/networkApi/AreaApiService.kt
@@ -1,2 +1,30 @@
 package android.example.com.taipeizoointrodemo.networkApi
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://data.taipei/api/v1/dataset/"
+
+// Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL) // server的url
+    .build()
+
+// Implement the MarsApiService interface with @GET getProperties returning a String
+interface TaipeiZooAeraApiService {
+    // 定義我們希望這個method使用的end point or path
+    // The end point for the JSON response is realestate
+    @GET("5a0e5fbb-72f8-41c6-908e-2fb25eff9b8a")
+    fun getProperties():
+            Call<String> // Call Object is used to start the request
+}
+
+// Create the MarsApi object using Retrofit to implement the MarsApiService
+object TaipeiZooAeraApi {
+    val retrofitService : TaipeiZooAeraApiService by lazy {
+        retrofit.create(TaipeiZooAeraApiService::class.java)
+    }
+}
--- app/src/main/java/android/example/com/taipeizoointrodemo/overview/OverviewViewModel.kt
+++ app/src/main/java/android/example/com/taipeizoointrodemo/overview/OverviewViewModel.kt
@@ -1,2 +1,48 @@
 package android.example.com.taipeizoointrodemo.overview
 
+import android.example.com.taipeizoointrodemo.networkApi.TaipeiZooAeraApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData String that stores the status of the most recent request
+    private val _response = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status String
+    val response: LiveData<String>
+        get() = _response
+
+    /**
+     * Call getMarsRealEstateProperties() on init so we can display status immediately.
+     */
+    init {
+        getTaipeiZooArea()
+    }
+
+    /**
+     * Sets the value of the status LiveData to the Mars API status.
+     */
+    private fun getTaipeiZooArea() {
+
+        // Call the MarsApi to enqueue the Retrofit request, implementing the callbacks
+        // 用enqueue是為了在start的時候是在background thread
+        TaipeiZooAeraApi.retrofitService.getProperties().enqueue( object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+                var qq = 0
+                qq++
+            }
+        })
+//        _response.value = "Set the Mars API Response here!"
+    }
+
+}
